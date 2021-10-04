85335e2c42d63f56dc3d64b1c9c64b0d28cf2673
Alex Castillo <wikitelmex@gmail.com>
2021-10-04 08:15:26 -0600

    module for arrays

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import _ from 'lodash';
 import './style.css';
+import { myLocalStorage } from "./modules/myLocalstorage";
 
 function component() {
   const element = document.createElement('div');
+  const myLS = new myLocalStorage;
+
+  myLS.save();
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
